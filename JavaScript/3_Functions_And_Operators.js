ccebe8d98447b457b88ec9efd7bf5c09c597d085
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-12-04 12:56:36 +0530

    initial commit

--- JavaScript/3_Functions_And_Operators.js
+++ JavaScript/3_Functions_And_Operators.js
@@ -13,3 +13,24 @@ it('should return a message', () => {
     assert(getMessage(), "it looks like nothing was returned from getMessage")
     assert.equal(typeof getMessage(), "string", "a string should be returned from getMessage");
 });
+
+
+
+//Operators
+function addTwo(input) {
+    return input+2;
+}
+
+module.exports = addTwo;
+
+//testing 
+const addTwo = require('../addTwo');
+const {assert} = require('chai');
+
+it('should add two', () => {
+    assert.equal(addTwo(2), 4);
+    assert.equal(addTwo(6), 8);
+    assert.equal(addTwo(10), 12);
+});
+
+
