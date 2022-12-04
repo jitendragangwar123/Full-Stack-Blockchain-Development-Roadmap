bd49c146cb603d14836b8028b622c035c6514cec
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-12-04 12:59:26 +0530

    initial commit

--- JavaScript/3_Functions_And_Operators.js
+++ JavaScript/3_Functions_And_Operators.js
@@ -33,4 +33,19 @@ it('should add two', () => {
     assert.equal(addTwo(10), 12);
 });
 
+// multiplying two numbers
+function product(a,b) {
+    return a*b;
+}
 
+module.exports = product;
+
+//testing 
+const product = require('../product');
+const {assert} = require('chai');
+
+it('should multiple two numbers', () => {
+    assert.equal(product(2,2), 4);
+    assert.equal(product(6,10), 60);
+    assert.equal(product(10,22), 220);
+});
