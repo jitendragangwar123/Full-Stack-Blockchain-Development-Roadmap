7f0929821da1a24c6123af5eb7577d8ba3f0452f
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-12-04 13:08:16 +0530

    initial commit

--- JavaScript/3_Functions_And_Operators.js
+++ JavaScript/3_Functions_And_Operators.js
@@ -69,3 +69,28 @@ it('should give the average of the numbers', () => {
     assert.equal(average(10,30,20,20), 20);
     assert.equal(average(0,2,7,3), 3);
 });
+
+// Math Library in JS
+function getRandom() {
+    return Math.random();
+
+}
+
+module.exports = getRandom;
+
+//testing 
+const getRandom = require('../getRandom');
+const {assert} = require('chai');
+
+it('should return a random number between 0 and 1', () => {
+    const num1 = getRandom();
+    assert.equal(typeof num1, "number");
+    assert.isAtLeast(num1, 0);
+    assert.isBelow(num1, 1);
+
+    const num2 = getRandom();
+    assert.equal(typeof num2, "number");
+    assert.notEqual(num2, num1, "Attempted to get the random number twice and they were the same");
+    assert.isAtLeast(num2, 0);
+    assert.isBelow(num2, 1);
+});
