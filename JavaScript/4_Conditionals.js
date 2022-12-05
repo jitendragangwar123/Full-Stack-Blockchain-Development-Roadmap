0ff527d0f08d0340757410277233222d4d189f03
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-12-05 11:25:44 +0530

    initial commit

--- JavaScript/4_Conditionals.js
+++ JavaScript/4_Conditionals.js
@@ -40,3 +40,36 @@ describe('isNotEqual', () => {
         });
     });
 });
+
+
+
+// if-else statements
+//isNotEqual.js
+function isNotEqual(a, b) {
+    if(a!==b){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+module.exports = isNotEqual;
+
+//testing 
+//test.js
+const isNotEqual = require('../isNotEqual');
+const { assert } = require('chai');
+
+describe('isNotEqual', () => {
+    describe('if the values are equal', () => {
+        it('should return false', () => {
+            assert.equal(isNotEqual(1, 1), false);
+        });
+    });
+    describe('if the values are not equal', () => {
+        it('should return true', () => {
+            assert.equal(isNotEqual(1, 2), true);
+        });
+    });
+});
