896fbfaa77a4c1a2ac18cce78dd4fcdd0fd04a22
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-12-08 00:48:44 +0530

    initial commit

--- JavaScript/6_Loops.js
+++ JavaScript/6_Loops.js
@@ -119,3 +119,33 @@ describe('scream', () => {
         assert.equal(scream(10), "aAaAaAaAaA");
     });
 });
+
+
+//toDouble.js
+function topDouble(value, top) {
+    while (value < top) {
+        value *= 2;
+    }
+    return value / 2;
+  
+}
+
+module.exports = topDouble;
+
+//test.js
+const { assert } = require('chai');
+const topDouble = require('../topDouble');
+
+describe('topDouble', () => {
+    it('should determine the top double of 1 less than 5', () => {
+        assert.equal(topDouble(1, 5), 4);
+    });
+
+    it('should determine the top double of 2 less than 100', () => {
+        assert.equal(topDouble(2, 100), 64);
+    });
+
+    it('should determine the top double of 5 less than 100', () => {
+        assert.equal(topDouble(5, 100), 80);
+    });
+});
