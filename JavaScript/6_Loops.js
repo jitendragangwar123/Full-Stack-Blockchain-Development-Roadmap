290361998ddd47e1187dbbe70a6efed65df49fb9
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-12-07 23:41:09 +0530

    initial commit

--- JavaScript/6_Loops.js
+++ JavaScript/6_Loops.js
@@ -60,3 +60,30 @@ describe('factorial', () => {
         assert.equal(factorial(5), 120);
     });
 });
+
+//scream.js
+function scream(n) {
+    let str="";
+    for(let i=0;i<n;i++){
+        str+="a";
+    }
+    return str;
+}
+
+module.exports = scream;
+
+//test.js
+const { assert } = require('chai');
+const scream = require('../scream');
+
+describe('scream', () => {
+    it('should do a scream of length 1', () => {
+        assert.equal(scream(1), "a");
+    });
+    it('should do a scream of length 5', () => {
+        assert.equal(scream(5), "aaaaa");
+    });
+    it('should do a scream of length 10', () => {
+        assert.equal(scream(10), "aaaaaaaaaa");
+    });
+});
