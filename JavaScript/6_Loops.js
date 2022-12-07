cb471527efc11bd28633a12750f1383e23b09107
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-12-07 23:37:05 +0530

    initial commit

--- JavaScript/6_Loops.js
+++ JavaScript/6_Loops.js
@@ -24,3 +24,39 @@ describe('summation', () => {
         assert.equal(summation(4), 10);
     });
 });
+
+//Factorial.js
+function factorial(n) {
+   let res=1; 
+   for(let i=1;i<=n;i++){
+       res*=i;
+
+   } 
+   return res;
+}
+
+module.exports = factorial;
+
+//test.js
+const { assert } = require('chai');
+const factorial = require('../factorial');
+
+describe('factorial', () => {
+    it('should determine factorial of 2', () => {
+        if(factorial(2) === undefined) {
+            assert(false, "Don't forget to return your result!");
+        }
+        else {
+            assert.equal(factorial(2), 2);
+        }
+    });
+    it('should determine factorial of 3', () => {
+        assert.equal(factorial(3), 6);
+    });
+    it('should determine factorial of 4', () => {
+        assert.equal(factorial(4), 24);
+    });
+    it('should determine factorial of 5', () => {
+        assert.equal(factorial(5), 120);
+    });
+});
