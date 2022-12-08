9b46e992fdd89d4c4c6325f1eded1d0d6074195f
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-12-08 23:24:09 +0530

    initial commit

--- JavaScript/7_CodingProblems.js
+++ JavaScript/7_CodingProblems.js
@@ -144,3 +144,36 @@ describe('checkNumber', () => {
     });
 })
 
+//maxSum
+//index.js
+function maxSum(num) {
+    let sum=0;
+    for(let i=1;i<=num;i++){
+        sum+=i;
+    }
+    return sum;
+}
+
+module.exports = maxSum;
+
+//test.js
+const { assert } = require('chai');
+const maxSum = require('../maxSum');
+
+describe('maxSum', () => {
+    it('should return zero', () => {
+        assert.equal(maxSum(0), 0);
+    });
+
+    it('should return the max sum of 1', () => {
+        assert.equal(maxSum(1), 1);
+    });
+
+    it('should return the max sum of positive integer', () => {
+        assert.equal(maxSum(5), 15);
+    });
+
+    it('should return zero for a negative integer', () => {
+        assert.equal(maxSum(-5), 0)
+    })
+})
