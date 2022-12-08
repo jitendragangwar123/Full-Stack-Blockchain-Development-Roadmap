ffa36442eead33d75e22dd34928de412bd188b3c
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-12-08 23:29:26 +0530

    initial commit

--- JavaScript/7_CodingProblems.js
+++ JavaScript/7_CodingProblems.js
@@ -177,3 +177,32 @@ describe('maxSum', () => {
         assert.equal(maxSum(-5), 0)
     })
 })
+
+//String Manipulation
+function startsWithX(string) {
+    if(string[0]==='x'){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+module.exports = startsWithX;
+
+//test.js
+const startsWithX = require('../startsWithX');
+const { assert } = require('chai');
+
+describe('startsWithX', () => {
+    it('should return true for a string starting with x', () => {
+        assert.equal(startsWithX("x"), true);
+        assert.equal(startsWithX("xpizza"), true);
+    });
+
+    it('should return false for a string not starting with x', () => {
+        assert.equal(startsWithX("pizza"), false);
+        assert.equal(startsWithX("zyx"), false);
+    });
+});
+
