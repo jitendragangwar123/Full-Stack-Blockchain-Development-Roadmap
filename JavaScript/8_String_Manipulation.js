586085abb2410e642f0dd5854eda2436f048ae50
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-12-09 17:37:08 +0530

    initial commit

--- JavaScript/8_String_Manipulation.js
+++ JavaScript/8_String_Manipulation.js
@@ -94,3 +94,38 @@ describe('endsWithX', () => {
     });
 });
 
+//index.js
+function isAllX(string) {
+    string=string.toUpperCase();
+    for(let i=0;i<string.length;i++){
+        if(string[i]!==string[0]){
+            return false;
+        }
+    }
+    return true;
+}
+module.exports = isAllX;
+
+//test.js
+const isAllX = require('../isAllX');
+const { assert } = require('chai');
+
+describe('isAllX', () => {
+    it('should return true for all lower case x', () => {
+        assert.equal(isAllX("xxxx"), true);
+    });
+
+    it('should return true for all upper case x', () => {
+        assert.equal(isAllX("X"), true);
+    });
+
+    it('should return true for a mix of x casing', () => {
+        assert.equal(isAllX("XxXxXXXxx"), true);
+    });
+
+    it('should return false for non-x strings', () => {
+        assert.equal(isAllX("Xxxpizza"), false);
+        assert.equal(isAllX("xPizzaX"), false);
+        assert.equal(isAllX("XxxxQxxxX"), false);
+    });
+});
