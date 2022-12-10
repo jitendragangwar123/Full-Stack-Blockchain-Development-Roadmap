eafe6ed696f1b330e92bf1691840b63f4227825d
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-12-11 02:28:04 +0530

    initial commit

--- JavaScript/11_CodingProblems.js
+++ JavaScript/11_CodingProblems.js
@@ -225,3 +225,33 @@ describe('isPalindrome', () => {
     })
 })
 
+//Sum together
+function sumTogether(arr1, arr2) {
+    let newArray=[];
+    for(let i=0;i<arr1.length;i++){
+        newArray[i]=arr1[i]+arr2[i];
+    }
+    return newArray;
+}
+
+module.exports = sumTogether;
+
+//test.js
+const {assert} = require('chai');
+const sumTogether = require('../sumTogether');
+
+describe('sumTogether', () => {
+    it('should return an empty array', () => {
+        const arr1 = [];
+        const arr2 = [];
+        const result = sumTogether(arr1, arr2);
+        assert.deepEqual(result, []);
+    })
+
+    it('should return an array of summed elements', () => {
+        const arr1 = [10, 20, 30];
+        const arr2 = [15, 25, 35];
+        const result = sumTogether(arr1, arr2);
+        assert.deepEqual(result, [25, 45, 65]);
+    })
+})
