c0ae75aba9c177d4617c00eccb0eb4e9bd50f7ff
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-12-10 12:46:38 +0530

    initial commit

--- JavaScript/10_Objects.js
+++ JavaScript/10_Objects.js
@@ -152,3 +152,63 @@ describe('ORDER_TYPES', () => {
         });
     });
 });
+
+
+//orderTypes.js
+const ORDER_TYPES = {
+    PIZZA : 0,
+    WINGS : 1,
+    SALAD : 3,
+    BURGER : 4,
+    FRIES : 6,
+    SANDWICH : 7
+}
+
+module.exports = ORDER_TYPES;
+
+//numberOfPizzas.js
+const ORDER_TYPES = require('./orderTypes');
+
+function numberOfPizzas(orders) {
+    let sum=0;
+    for(let i=0;i<orders.length;i++){
+        if(orders[i].type===ORDER_TYPES.PIZZA){
+            sum+=orders[i].pizzas;
+        }
+    }
+    return sum;
+}
+
+module.exports = numberOfPizzas;
+
+//test.js
+const numberOfPizzas = require('../numberOfPizzas');
+const { assert } = require('chai');
+const ORDER_TYPES = require('../orderTypes');
+
+describe('numberOfPizzas', () => {
+    describe('just pizza', () => {
+        const orders = [
+            { pizzas: 3, type: ORDER_TYPES.PIZZA, extraCheese: true },
+            { pizzas: 5, type: ORDER_TYPES.PIZZA, extraCheese: false },
+            { pizzas: 10, type: ORDER_TYPES.PIZZA, extraCheese: true },
+        ]
+
+        it('should return the number of pizzas', () => {
+            assert.equal(numberOfPizzas(orders), 18);
+        });
+    });
+
+    const keys = Object.keys(ORDER_TYPES);
+    describe(keys.join(', '), () => {
+        const orders = keys.reduce((arr, key) => {
+            return arr.concat({
+                type: key,
+            });
+        }, [{ pizzas: 3, type: ORDER_TYPES.PIZZA }]);
+
+        it('should return the number of pizzas', () => {
+            assert.equal(numberOfPizzas(orders), 3);
+        });
+    });
+});
