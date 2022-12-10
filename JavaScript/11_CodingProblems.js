e02358b720cdba3386618f96a5fdf7975597d264
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-12-11 00:16:19 +0530

    initial commit

--- JavaScript/11_CodingProblems.js
+++ JavaScript/11_CodingProblems.js
@@ -96,3 +96,52 @@ describe('countC', () => {
         assert.equal(countC('Circus'), 2);
     });
 })
+
+//countVowels.js
+const Vowels=['a','e','i','o','u'];
+function countVowels(str) {
+    let count=0;
+    for(let i=0;i<str.length;i++){
+        if(Vowels.includes(str[i].toLowerCase())){
+            count++;
+        }
+    }
+    return count;
+}
+
+module.exports = countVowels;
+
+//test.js
+const { assert } = require('chai');
+const countVowels = require('../countVowels');
+
+describe('countVowels', () => {
+    it('should return zero', () => {
+        assert.equal(countVowels('bczx'), 0);
+    });
+
+    it('should count all vowels', () => {
+        assert.equal(countVowels("Igloo"), 3)
+    })
+
+    describe('uppercase vowels', () => {
+        it('should count all uppercase vowels', () => {
+            assert.equal(countVowels('AEIOU'), 5);
+        });
+
+        it('should only count vowels', () => {
+            assert.equal(countVowels('APPLE'), 2);
+        });
+    })
+    
+
+    describe('lowercase vowels', () => {
+        it('should count all lowercase vowels', () => {
+            assert.equal(countVowels('aeiou'), 5);
+        });
+
+        it('should only count vowels', () => {
+            assert.equal(countVowels('apple'), 2);
+        });
+    })
+})
