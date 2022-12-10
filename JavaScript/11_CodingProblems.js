5e652910a173c5e034d4c33413ecfa8b4af3f1bc
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-12-10 18:58:57 +0530

    initial commit

--- JavaScript/11_CodingProblems.js
+++ JavaScript/11_CodingProblems.js
@@ -22,3 +22,45 @@ describe('shortestString', () => {
         assert.equal(shortestString('lion', 'gazelle'), 'lion');
     });
 })
+
+
+//index.js
+function halfValue(numbers) {
+    let newArray=[];
+    for(let i=0;i<numbers.length;i++){
+        newArray[i]=Math.round(numbers[i]/2);
+    }
+    return newArray;
+}
+
+module.exports = halfValue;
+
+//test.js
+const {assert} = require('chai');
+const halfValue = require("../halfValue");
+
+const modifyErr = "Whoops! Create a new array, do not modify the original array.";
+const compare = (a,b) => JSON.stringify(a) === JSON.stringify(b);
+
+describe('halfValue', () => {
+    it('should return an empty array', () => {
+        const arr = [];
+        const actual = halfValue(arr);
+        assert(compare(arr, []), modifyErr);
+        assert.sameMembers(actual, []);
+    });
+
+    it('should return an array of even value', () => {
+        const arr = [2, 4, 6, 8];
+        const actual = halfValue(arr);
+        assert(compare(arr, [2, 4, 6, 8]), modifyErr);
+        assert.sameOrderedMembers(actual, [1, 2, 3, 4]);
+    });
+
+    it('should return an array of odd values', () => {
+        const arr = [11, 13, 15, 17];
+        const actual = halfValue(arr);
+        assert(compare(arr, [11, 13, 15, 17]), modifyErr);
+        assert.sameOrderedMembers(actual, [6, 7, 8, 9]);
+    });
+})
