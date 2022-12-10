a0175f0056bd867b676d926c8951d2a766484223
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-12-11 03:06:44 +0530

    initial commit

--- JavaScript/11_CodingProblems.js
+++ JavaScript/11_CodingProblems.js
@@ -255,3 +255,40 @@ describe('sumTogether', () => {
         assert.deepEqual(result, [25, 45, 65]);
     })
 })
+
+
+//Count the Elements 
+function countElements(elements) {
+    let elementsCount={};
+    for(let i=0;i<elements.length;i++){
+        if(!elementsCount[elements[i]]){
+            elementsCount[elements[i]]=1;
+        }
+        else{
+            elementsCount[elements[i]]+=1;
+        }
+    }
+    return elementsCount;
+
+}
+
+module.exports = countElements;
+
+
+//test.js
+const {assert} = require('chai');
+const countElements = require('../countElements');
+
+describe('countElements', () => {
+    it('should return an empty object', () => {
+        const elements = [];
+        const result = countElements(elements);
+        assert.deepEqual(result, {});
+    })
+
+    it('should return an object of non-duplicative element counts', () => {
+        const elements = ["a", "a", "b", "c", "b", "d"];
+        const result = countElements(elements);
+        assert.deepEqual(result, {a: 2, b: 2, c: 1, d: 1})
+    })
+})
