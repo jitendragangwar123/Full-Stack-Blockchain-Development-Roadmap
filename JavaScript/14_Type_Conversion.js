2f101a2e37eefecc201118dc7ca25b2a31f77c73
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-12-12 01:48:35 +0530

    initial commit

--- JavaScript/14_Type_Conversion.js
+++ JavaScript/14_Type_Conversion.js
@@ -220,3 +220,42 @@ describe('toJSON', () => {
         assert.equal(toJSON({ a: 2 }), '{"a":2}');
     });
 });
+
+//Valid JSON
+const personJSON = `
+    {
+        "name":"Jitendra",
+        "age": 24,
+        "isReal": true
+    } 
+`;
+//const person = JSON.parse(personJSON);
+//console.log(person.name); // "Jitendra"
+
+module.exports = personJSON;
+
+//test.js
+const personJSON = require('../personJSON');
+const { assert } = require('chai');
+
+let person;
+describe('personJSON', () => {
+    before(() => {
+        person = JSON.parse(personJSON);
+    });
+
+    it('should have a property name that is a string', () => {
+        assert(person.hasOwnProperty('name'), "The resulting object did not have a property `name`");
+        assert.equal(typeof person.name, "string");
+    });
+
+    it('should have a property age that is a number', () => {
+        assert(person.hasOwnProperty('age'), "The resulting object did not have a property `age`");
+        assert.equal(typeof person.age, "number");
+    });
+
+    it('should have a property isReal that is a boolean', () => {
+        assert(person.hasOwnProperty('isReal'), "The resulting object did not have a property `isReal`");
+        assert.equal(typeof person.isReal, "boolean");
+    });
+});
