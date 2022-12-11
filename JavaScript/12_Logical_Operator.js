b8b6caefc3615c71e4d3f71eef54f97049e1155c
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-12-11 11:45:35 +0530

    initial commit

--- JavaScript/12_Logical_Operator.js
+++ JavaScript/12_Logical_Operator.js
@@ -33,3 +33,30 @@ describe('willEat', () => {
         });
     });
 });
+
+//Default Operator "||"
+function double(x) {
+    const res= (x || 0) * 2;
+    return res;
+}
+
+module.exports = double;
+
+//test.js
+const double = require('../double');
+const { assert } = require('chai');
+
+describe('configMessage', () => {
+    describe('when there is a number passed', () => {
+        it('should double it', () => {
+            assert.equal(double(2), 4, "Did not return the double of 2!");
+            assert.equal(double(4), 8, "Did not return the double of 4!");
+            assert.equal(double(5), 10, "Did not return the double of 5!");
+        });
+    });
+    describe('when there is no number passed', () => {
+        it('should default to zero', () => {
+            assert.equal(double(), 0, "Passing undefined for x should return 0");
+        });
+    });
+});
