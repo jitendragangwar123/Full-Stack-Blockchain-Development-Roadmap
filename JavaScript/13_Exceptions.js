594f132bc262a9d23cf325b3b97fdc5f17458a24
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-12-11 23:24:04 +0530

    initial commit

--- JavaScript/13_Exceptions.js
+++ JavaScript/13_Exceptions.js
@@ -134,4 +134,32 @@ describe('catchError', () => {
     });
 });
 
+//index.js
+function startError() {
+    let x=0;
+    x(); // TypeError
+    new array(Infinity); // Range Error
+    x.72; //SyntaxError
+    
+}
 
+module.exports = startError;
+
+//test.js
+const { assert } = require('chai');
+const startError = require('../startError');
+
+describe('startError', () => {
+    it('should throw a subclass of error', () => {
+        let ex;
+        try {
+            startError();
+        }
+        catch (_ex) {
+            ex = _ex;
+        }
+        assert(ex, "did not throw an error");
+        console.log(ex);
+        assert.equal(ex.__proto__.__proto__.constructor, Error);
+    });
+});
