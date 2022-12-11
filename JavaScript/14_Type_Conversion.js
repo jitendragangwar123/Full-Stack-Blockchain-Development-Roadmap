dba0fe5b81c2b2c770ff1e522199b5db6f8ff3fe
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-12-12 01:01:23 +0530

    initial commit

--- JavaScript/14_Type_Conversion.js
+++ JavaScript/14_Type_Conversion.js
@@ -52,3 +52,37 @@ describe('toNumber', () => {
         });
     });
 });
+
+
+//toString()
+function combineToString(a, b) {
+    return (a+""+b);
+}
+
+module.exports = combineToString;
+
+//test.js
+const combineToString = require('../combineToString');
+const { assert } = require('chai');
+
+describe('combineToString', () => {
+    describe('given two numbers', () => {
+        it('should combine them', () => {
+            assert.strictEqual(combineToString(1,1), "11");
+            assert.strictEqual(combineToString(5,10), "510");
+        });
+    });
+    describe('given a number and a string', () => {
+        it('should combine them', () => {
+            assert.strictEqual(combineToString(2, "2"), "22");
+            assert.strictEqual(combineToString("5", 55), "555");
+        });
+    });
+    describe('given a number and a boolean', () => {
+        it('should combine them', () => {
+            assert.strictEqual(combineToString(2, true), "2true");
+            assert.strictEqual(combineToString(false, 55), "false55");
+        });
+    });
+});
+
