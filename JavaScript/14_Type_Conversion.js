1cb72494596d0ec0ba1acf0625e40044d0c6bb32
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-12-12 01:23:37 +0530

    initial commit

--- JavaScript/14_Type_Conversion.js
+++ JavaScript/14_Type_Conversion.js
@@ -139,3 +139,53 @@ describe('isTruthy', () => {
         });
     });
 });
+
+
+//Loose Equals("==") and strict Equals("===")
+function looseEquals(a, b) {
+    if(a==b){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+module.exports = looseEquals;
+
+//test.js
+const looseEquals = require('../looseEquals');
+const { assert } = require('chai');
+
+describe('looseEquals', () => {
+    describe('comparing things of the same type', () => {
+        describe('same value', () => {
+            it('should be equal', () => {
+                assert(looseEquals(1, 1));
+                assert(looseEquals("a", "a"));
+                assert(looseEquals(false, false));
+            });
+        });
+        describe('different value', () => {
+            it('should not be equal', () => {
+                assert.isNotTrue(looseEquals(0, 1));
+                assert.isNotTrue(looseEquals("a", "c"));
+                assert.isNotTrue(looseEquals(false, true));
+            });
+        });
+    });
+    describe('comparing things of a different type', () => {
+        describe('same loose value', () => {
+            it('should be equal', () => {
+                assert(looseEquals(1, true));
+                assert(looseEquals("2", 2));
+            });
+        });
+        describe('different value', () => {
+            it('should not be equal', () => {
+                assert.isNotTrue(looseEquals(0, true));
+                assert.isNotTrue(looseEquals("5", 10));
+            });
+        });
+    });
+});
