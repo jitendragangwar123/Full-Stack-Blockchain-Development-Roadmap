bc1b5a0ffea1faa7a7612a479512548eac322525
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-12-11 23:12:54 +0530

    initial commit

--- JavaScript/13_Exceptions.js
+++ JavaScript/13_Exceptions.js
@@ -76,3 +76,62 @@ describe('catchError', () => {
     });
 });
 
+/*
+function writeLogFile(id, contents) {
+    try {
+        writeFile(`logs/${id}`, contents);
+    }
+    catch(ex) {
+        return false;
+    }
+    return true;
+}
+*/
+
+//index.js
+function catchError(fn) {
+    try{
+        fn();
+    }
+    catch(ex){
+        return ex;
+
+    }
+    return false;
+}
+
+module.exports = catchError;
+
+//test.js
+const { assert } = require('chai');
+const catchError = require('../catchError');
+
+describe('catchError', () => {
+    it('should catch a thrown error', () => {
+        let ex;
+        try { 
+            catchError(() => {
+                throw new Error();
+            });
+        }
+        catch(_ex) {
+            ex = _ex;
+        }
+        assert(!ex, "the error should have been caught");
+    });
+    
+    it('should return the thrown error', () => {
+        let expected = new Error();
+        let actual = catchError(() => {
+            throw expected;
+        });
+        assert.equal(actual, expected, "Should have returned the same error that was caught");
+    });
+
+    it('should return false when no error is thrown', () => {
+        let ex = catchError(() => {});
+        assert.equal(ex, false);
+    });
+});
+
+
