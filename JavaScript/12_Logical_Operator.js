a80a7256c37c0fbef0b9a388a57d283a7bef8b82
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-12-11 12:44:34 +0530

    initial commit

--- JavaScript/12_Logical_Operator.js
+++ JavaScript/12_Logical_Operator.js
@@ -144,4 +144,37 @@ describe('friendName', () => {
     });
 });
 
+// NOT operator 
+function carCrossing(aCrossing, bCrossing) {
+    return (aCrossing && !bCrossing) || (!aCrossing && bCrossing);
+}
 
+module.exports = carCrossing;
+
+//test.js
+//Exclusive OR
+const carCrossing = require('../carCrossing');
+const { assert } = require('chai');
+
+describe('carCrossing', () => {
+    describe('when a is crossing and not b', () => {
+        it('should allow a to cross', () => {
+            assert.equal(carCrossing(true, false), true);
+        });
+    });
+    describe('when both are crossing', () => {
+        it('should be stalemate', () => {
+            assert.equal(carCrossing(true, true), false);
+        });
+    });
+    describe('when both are crossing', () => {
+        it('should allow b to cross', () => {
+            assert.equal(carCrossing(false, true), true);
+        });
+    });
+    describe('when neither are crossing', () => {
+        it('should not be true', () => {
+            assert.equal(carCrossing(false, false), false);
+        });
+    });
+});
