e300ebc757d409a5517f5ef35f6797f91c39d43a
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-12-11 12:02:18 +0530

    initial commit

--- JavaScript/12_Logical_Operator.js
+++ JavaScript/12_Logical_Operator.js
@@ -60,3 +60,45 @@ describe('configMessage', () => {
         });
     });
 });
+
+// "&&" Operator
+function canBreathe(isConnected, hasOxygen, aboveWater) {
+    return (isConnected && hasOxygen || aboveWater)
+        
+}
+
+module.exports = canBreathe;
+
+//test.js
+const canBreathe = require('../canBreathe');
+const { assert } = require('chai');
+
+describe('canBreathe', () => {
+    describe('when above water', () => {
+        it('should be able to breathe', () => {
+            assert.equal(canBreathe(false, false, true), true);
+            assert.equal(canBreathe(true, false, true), true);
+            assert.equal(canBreathe(false, true, true), true);
+            assert.equal(canBreathe(true, true, true), true);
+        });
+    });
+    describe('when not above water', () => {
+        describe('and not connected', () => {
+            it('should not be able to breathe', () => {
+                assert.equal(canBreathe(true, false, false), false);
+                assert.equal(canBreathe(false, false, false), false);
+            });
+        });
+        describe('and there is no oxygen', () => {
+            it('should not be able to breathe', () => {
+                assert.equal(canBreathe(false, true, false), false);
+                assert.equal(canBreathe(false, false, false), false);
+            });
+        });
+        describe('and is connected with oxygen', () => {
+            it('should be able to breathe', () => {
+                assert.equal(canBreathe(true, true, false), true);
+            });
+        });
+    });
+});
