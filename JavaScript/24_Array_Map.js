3ce5e74b4fa3f5a75f9177860a53f5e22e2258c5
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-12-16 03:31:01 +0530

    initial commit

--- JavaScript/24_Array_Map.js
+++ JavaScript/24_Array_Map.js
@@ -36,3 +36,26 @@ describe('plus one', () => {
         assert.sameOrderedMembers([1, 2, 3], plusOne([0, 1, 2]));
     });
 });
+
+//Square Root
+//squareRoot.js
+function squareRoot(arr) {
+    return arr.map((x)=>Math.sqrt(x));
+}
+
+module.exports = squareRoot;
+
+
+//test.js
+const squareRoot = require('../squareRoot');
+const {assert} = require('chai');
+
+describe('squareRoot', () => {
+    it('should take the sqrt for a single element', () => {
+        assert.sameOrderedMembers([1], squareRoot([1]));
+    });
+
+    it('should take the sqrt for several elements', () => {
+        assert.sameOrderedMembers([1, 2, 3, 4], squareRoot([1, 4, 9, 16]));
+    });
+});
