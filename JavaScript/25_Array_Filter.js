3eb309e214160fbcdb4444cb6572eb8ea6cb171d
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-12-16 04:24:58 +0530

    initial commit

--- JavaScript/25_Array_Filter.js
+++ JavaScript/25_Array_Filter.js
@@ -63,3 +63,32 @@ describe('only true', () => {
         assert.sameMembers(expected, actual);
     });
 });
+
+
+//shortString.js
+function shortStrings(array) {
+    return array.filter((str)=>{
+        return (str.length<=3);
+    });
+}
+
+module.exports = shortStrings;
+
+//test.js
+const { assert } = require('chai');
+const shortStrings = require('../shortStrings');
+
+describe('shortStrings', () => {
+    it('should filter a few strings', () => {
+        const expected = ["abc", "a"];
+        const actual = shortStrings(["abc", "abcd", "a"]);
+        console.log({expected, actual});
+        assert.sameMembers(expected, actual);
+    });
+    it('should filter many strings', () => {
+        const expected = ["a", "b", "bat", "cde", "f"];
+        const actual = shortStrings(["a", "apples", "b", "bats", "bat", "pizza", "cde", "f"]);
+        console.log({expected, actual});
+        assert.sameMembers(expected, actual);
+    });
+});
