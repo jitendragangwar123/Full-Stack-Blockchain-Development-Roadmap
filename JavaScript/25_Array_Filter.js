f03f8553e8a7bcb58e6ea4b2c83f58c65db75604
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-12-16 04:29:15 +0530

    initial commit

--- JavaScript/25_Array_Filter.js
+++ JavaScript/25_Array_Filter.js
@@ -92,3 +92,50 @@ describe('shortStrings', () => {
         assert.sameMembers(expected, actual);
     });
 });
+
+/*
+Filtering Objects:- 
+        We can filter objects, just like we filter numbers, booleans and strings!
+ */
+
+//topStudents.js
+function topStudents(array) {
+    return array.filter((student)=>{
+        return (student.grade>=90);
+    });
+}
+
+module.exports = topStudents;
+
+//test.js
+const { assert } = require('chai');
+const topStudents = require('../topStudents');
+
+describe('topStudents', () => {
+    it('should filter a few students', () => {
+        const expected = [{ name: 'David', grade: 90 }, { name: 'Daisy', grade: 100 }];
+        const actual = topStudents([{ name: 'Darcie', grade: 80 }, { name: 'David', grade: 90 }, { name: 'Daisy', grade: 100 }]);
+        console.log({expected, actual});
+        assert.sameDeepMembers(expected, actual);
+    });
+    it('should filter many students', () => {
+        const expected = [
+            { name: 'David', grade: 90 }, 
+            { name: 'Darcie', grade: 100 },
+            { name: 'Percy', grade: 99 },
+            { name: 'Steve', grade: 95 }
+        ];
+        const actual = topStudents([
+            { name: 'Darcie', grade: 100 }, 
+            { name: 'Daisy', grade: 80 }, 
+            { name: 'David', grade: 90 },
+            { name: 'Steve', grade: 95},
+            { name: 'James', grade: 82 },
+            { name: 'Percy', grade: 99 },
+            { name: 'Luke', grade: 68 },
+            { name: 'Kevin', grade: 72 }
+        ]);
+        console.log({expected, actual});
+        assert.sameDeepMembers(expected, actual);
+    });
+});
