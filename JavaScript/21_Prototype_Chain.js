f5db33bcf949434e19865ffb2425dea4524de6d7
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-12-15 22:17:43 +0530

    initial commit

--- JavaScript/21_Prototype_Chain.js
+++ JavaScript/21_Prototype_Chain.js
@@ -241,3 +241,64 @@ describe('Circle', () => {
         });
     });
 }); 
+
+
+//Rectangle.js
+const Shape = require('./Shape');
+
+function Rectangle(x, y, height, width) {
+    Shape.call(this,x,y);
+    this.height=height;
+    this.width=width;
+}
+
+Rectangle.prototype=Object.create(Shape.prototype);
+
+module.exports = Rectangle;
+
+//Shape.js
+// Our Shape "Constructor"
+function Shape(x, y) {
+    // store x and y in this.position
+    this.position={x,y};
+}
+
+Shape.prototype.move=function(x,y){
+    this.position.x+=x;
+    this.position.y+=y;
+    
+}
+module.exports = Shape;
+
+//test.js
+const { assert } = require('chai');
+const Rectangle = require('../Rectangle');
+const Shape = require('../Shape');
+
+describe('Rectangle', () => {
+    let rectangle;
+    let x = 10;
+    let y = 15;
+    let height = 15;
+    let width = 20;
+    describe('instance', () => {
+        beforeEach(() => {
+            rectangle = new Rectangle(x, y, height, width);
+        });
+
+        it('should set the properties', () => {
+            assert.equal(rectangle.position.x, x);
+            assert.equal(rectangle.position.y, y);
+            assert.equal(rectangle.height, height);
+            assert.equal(rectangle.width, width);
+        });
+
+        it('should have a move function', () => {
+            assert.equal(typeof rectangle.move, "function", "Could not find the function move on the rectangle instance");
+        });
+
+        it('should refer to the Shape prototype in the prototype chain', () => {
+            assert(Shape.prototype.isPrototypeOf(rectangle));
+        });
+    });
+}); 
