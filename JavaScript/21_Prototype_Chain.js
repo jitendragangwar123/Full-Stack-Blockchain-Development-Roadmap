40a2b7fb7e09370ad89dd8175357b29878b21bb2
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-12-15 21:58:27 +0530

    initial commit

--- JavaScript/21_Prototype_Chain.js
+++ JavaScript/21_Prototype_Chain.js
@@ -71,3 +71,102 @@ describe('Shape', () => {
         });
     });
 }); 
+
+
+/*
+Move Shape :- 
+By adding this method to the Shape.prototype object, new instances will be able to access it via the prototype chain.
+*/
+
+//Shape.js
+// Our Shape "Constructor"
+function Shape(x, y) {
+    // store x and y in this.position
+    this.position={x,y};
+}
+
+Shape.prototype.move=function(x,y){
+    this.position.x+=x;
+    this.position.y+=y;
+    
+}
+module.exports = Shape;
+
+//test.js
+const { assert } = require('chai');
+const Shape = require('../Shape');
+
+let shape;
+let x = 5;
+let y = 10;
+let moveX = 20;
+let moveY = 30;
+describe('Shape Instance', () => {
+    beforeEach(() => {
+        shape = new Shape(x, y);
+    });
+
+    it('should set the initial position coordinates', () => {
+        assert.equal(shape.position.x, x);
+        assert.equal(shape.position.y, y);
+    });
+
+    it('should have a function called move', () => {
+        assert.equal(typeof shape.move, "function");
+    });
+
+    it('should handle a move', () => {
+        shape.move(moveX, moveY);
+        assert.equal(shape.position.x, x + moveX);
+        assert.equal(shape.position.y, y + moveY);
+    });
+});
+
+//Circle.js
+const Shape = require('./Shape');
+
+function Circle(x, y, radius) {
+    Shape.call(this,x,y);
+    // store radius on this
+    this.radius=radius;
+    
+}
+
+module.exports = Circle;
+
+//Shape.js
+// Our Shape "Constructor"
+function Shape(x, y) {
+    // store x and y in this.position
+    this.position={x,y};
+}
+
+Shape.prototype.move=function(x,y){
+    this.position.x+=x;
+    this.position.y+=y;
+    
+}
+module.exports = Shape;
+
+//test.js
+const { assert } = require('chai');
+const Circle = require('../Circle');
+
+describe('Circle', () => {
+    let circle;
+    let x = 10;
+    let y = 15;
+    let radius = 15;
+    describe('instance', () => {
+        beforeEach(() => {
+            circle = new Circle(x, y, radius);
+        });
+
+        it('should set the properties', () => {
+            assert.equal(circle.position.x, x);
+            assert.equal(circle.position.y, y);
+            assert.equal(circle.radius, radius);
+        });
+    });
+}); 
+
