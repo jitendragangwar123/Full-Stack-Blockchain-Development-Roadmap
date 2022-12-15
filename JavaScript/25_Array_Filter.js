519a8e7d01dd8dfaa0952fe115b34e682dbbc6e6
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-12-16 04:22:28 +0530

    initial commit

--- JavaScript/25_Array_Filter.js
+++ JavaScript/25_Array_Filter.js
@@ -35,3 +35,31 @@ describe('less than five', () => {
         assert.sameMembers(expected, actual);
     });
 });
+
+//onlyTrue.js
+function onlyTrue(array) {
+    return array.filter((x)=>{
+        return x;
+    });
+}
+
+module.exports = onlyTrue;
+
+//test.js
+const { assert } = require('chai');
+const onlyTrue = require('../onlyTrue');
+
+describe('only true', () => {
+    it('should filter all false values', () => {
+        const expected = [];
+        const actual = onlyTrue([false, false, false]);
+        console.log({expected, actual});
+        assert.sameMembers(expected, actual);
+    });
+    it('should filter mixed false and true values', () => {
+        const expected = [true, true, true];
+        const actual = onlyTrue([false, true, true, false, true]);
+        console.log({expected, actual});
+        assert.sameMembers(expected, actual);
+    });
+});
