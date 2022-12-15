94b372243493be20300e1da5ad528a0814b242a1
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-12-16 02:10:45 +0530

    initial commit

--- JavaScript/23_Array_Sort.js
+++ JavaScript/23_Array_Sort.js
@@ -64,3 +64,31 @@ describe('sortUp', () => {
     });
 });
 
+
+//Sorting Descending
+//sortDown.js
+function sortDown(array) {
+    array.sort((a,b)=>b-a);
+}
+
+module.exports = sortDown;
+
+//test.js
+const { assert } = require('chai');
+const sortDown = require('../sortDown');
+
+describe('sortDown', () => {
+    it('should mantain sort order for sorted elements', () => {
+        let actual = [15, 10, 5];
+        sortDown(actual);
+        const expected = [15, 10, 5];
+        assert.sameOrderedMembers(actual, expected);
+    });
+
+    it('should fix sort order for unsorted elements', () => {
+        let actual = [7, 4, 2, 3, 0, 5];
+        sortDown(actual);
+        const expected = [7, 5, 4, 3, 2, 0];
+        assert.sameOrderedMembers(actual, expected);
+    });
+});
