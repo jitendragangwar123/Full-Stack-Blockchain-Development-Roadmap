d2572bccb10b2296d836e329714d32cfc7330ad0
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-12-15 23:04:52 +0530

    initial commit

--- JavaScript/22_Classes_and_Methods.js
+++ JavaScript/22_Classes_and_Methods.js
@@ -50,3 +50,65 @@ describe('new Hero', () => {
         assert.equal(hero.health, 50);
     });
 });
+
+
+/*
+Methods:-
+        In addition to constructors we can define our own methods on classes:
+Ex:- 
+class Team {
+    constructor() {
+        this.wins = 0;
+        this.losses = 0;
+    }
+    changeRecord(isWin) {
+        if(isWin) {
+            this.wins++;
+        }
+        else {
+            this.losses++;
+        }
+    }
+}
+*/
+
+//Hero.js
+class Hero {
+    constructor() {
+        this.health=50;
+    }
+    takeDamage(damage){
+        this.health-=damage;
+    }
+}
+
+module.exports = Hero;
+
+//test.js
+const { assert } = require('chai');
+const Hero = require('../Hero');
+
+describe('new Hero', () => {
+    const hero = new Hero();
+
+    describe('after taking 5 damage', () => {
+        before(() => {
+            hero.takeDamage(5);
+        });
+
+        it('should have 45 health', () => {
+            assert.equal(hero.health, 45);
+        });
+
+        describe('after taking 20 more damage', () => {
+            before(() => {
+                hero.takeDamage(20);
+            });
+
+            it('should have 25 health', () => {
+                assert.equal(hero.health, 25);
+            });
+        });
+    });
+});
+
