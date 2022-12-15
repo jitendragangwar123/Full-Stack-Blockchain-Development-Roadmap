ac19387063af8f8a952132bdb0aff8257142c3ba
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-12-16 04:18:39 +0530

    initial commit

--- JavaScript/25_Array_Filter.js
+++ JavaScript/25_Array_Filter.js
@@ -7,3 +7,31 @@ const ones = [1,2,3,1,1].filter((function(el) {
 }));
 
 */
+
+//lessThanFive.js
+function lessThanFive(array) {
+    return array.filter((x)=>{
+        return (x<5);
+    });
+}
+
+module.exports = lessThanFive;
+
+//test.js
+const { assert } = require('chai');
+const lessThanFive = require('../lessThanFive');
+
+describe('less than five', () => {
+    it('should filter a few elements less than 5', () => {
+        const expected = [0, 2, 4];
+        const actual = lessThanFive([0, 2, 4, 6, 8]);
+        console.log({expected, actual});
+        assert.sameMembers(expected, actual);
+    });
+    it('should filter several elements less than 5', () => {
+        const expected = [1, 2, 3, 4];
+        const actual = lessThanFive([1, 2, 3, 4, 5, 6, 7, 8]);
+        console.log({expected, actual});
+        assert.sameMembers(expected, actual);
+    });
+});
