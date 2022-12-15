cf63090b0440be48a5a0335c20bbcf3179f17964
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-12-16 04:17:25 +0530

    initial commit

---
+++ JavaScript/25_Array_Filter.js
@@ -0,0 +1,9 @@
+/*
+Array Filter:-
+        When filtering arrays, we want to come up with some boolean condition (true/false) that will determine if we should keep the element or not.
+ex:-
+const ones = [1,2,3,1,1].filter((function(el) {
+    return (el === 1);
+}));
+
+*/
