048a55af6f548e6d6a17711ef8b7b4da28ecbc21
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-12-16 04:06:00 +0530

    initial commit

--- JavaScript/24_Array_Map.js
+++ JavaScript/24_Array_Map.js
@@ -124,3 +124,49 @@ describe('squaredMap', () => {
 });
 
 
+/*
+Mapping Over Objects :-
+
+Ex:-
+const users = [
+    { name: 'Corey', loggedIn: true },
+    { name: 'Anna', loggedIn: false }
+];
+const loggedOutUsers = users.map(function(user) {
+    return {
+        name: user.name,
+        loggedIn: false
+    }
+});
+*/
+
+//addScore.js
+function addScore(players) {
+    return players.map((player)=>{
+       player.score+=10;
+       return player;
+    });
+}
+
+module.exports = addScore;
+
+//test.js
+const addScore = require('../addScore');
+const { assert } = require('chai');
+
+describe('addScore', () => {
+    it('should add 10 to the score for a single participant', () => {
+        const actual = addScore([{ id: 1, score: 5 }]);
+        const expected = [{ id: 1, score: 15 }];
+        console.log({ actual, expected });
+        assert.sameDeepOrderedMembers(actual, expected);
+    });
+
+    it('should add 10 to the score for several participants', () => {
+        const actual = addScore([{ id: 1, score: 5 }, { id: 3, score: 10 }, { id: 2, score: 15 }]);
+        const expected = [{ id: 1, score: 15 }, { id: 3, score: 20 }, { id: 2, score: 25 }];
+        console.log({ actual, expected });
+        assert.sameDeepOrderedMembers(actual, expected);
+    });
+});
+
