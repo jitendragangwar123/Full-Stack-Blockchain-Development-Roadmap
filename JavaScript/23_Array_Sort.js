335fe0047445cd39af4a619ff4461eb8bf97e900
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-12-16 03:08:49 +0530

    initial commit

--- JavaScript/23_Array_Sort.js
+++ JavaScript/23_Array_Sort.js
@@ -229,3 +229,66 @@ describe('sortStudents', () => {
         assert.sameDeepOrderedMembers(actual, expected);
     });
 });
+
+
+//Sorting By Month
+//sortByMonth.js
+const MONTHS = [
+    'JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN',
+    'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'
+];
+
+function sortByMonth(events) {
+    events.sort((a,b)=>{
+    return MONTHS.indexOf(a.month)-MONTHS.indexOf(b.month);
+
+    });
+}
+
+module.exports = sortByMonth;
+
+//test.js
+const { assert } = require('chai');
+const sortByMonth = require('../sortByMonth');
+
+describe('sortByMonth', () => {
+    it('should fix the sort order for three events', () => {
+        let actual = [
+            { event: 'dance', month: 'MAR' },
+            { event: 'farmers market', month: 'JUN' },
+            { event: 'parade', month: 'JAN' },
+        ];
+        sortByMonth(actual);
+        const expected = [
+            { event: 'parade', month: 'JAN' },
+            { event: 'dance', month: 'MAR' },
+            { event: 'farmers market', month: 'JUN' },
+        ];
+        console.log({ actual, expected });
+        assert.sameDeepOrderedMembers(actual, expected);
+    });
+
+    it('should fix the sort order for many events', () => {
+        let actual = [
+            { event: 'haunted hayride', month: 'OCT' },
+            { event: 'holiday party', month: 'DEC' },
+            { event: 'picnic', month: 'SEP' },
+            { event: 'dance', month: 'MAR' },
+            { event: 'snowball fight', month: 'FEB' },
+            { event: 'farmers market', month: 'JUN' },
+            { event: 'parade', month: 'JAN' },
+        ];
+        sortByMonth(actual);
+        const expected = [
+            { event: 'parade', month: 'JAN' },
+            { event: 'snowball fight', month: 'FEB' },
+            { event: 'dance', month: 'MAR' },
+            { event: 'farmers market', month: 'JUN' },
+            { event: 'picnic', month: 'SEP' },
+            { event: 'haunted hayride', month: 'OCT' },
+            { event: 'holiday party', month: 'DEC' },
+        ];
+        console.log({ actual, expected });
+        assert.sameDeepOrderedMembers(actual, expected);
+    });
+});
