18d2a057e306d7906260d78972db581b479bf016
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-12-16 02:31:05 +0530

    initial commit

--- JavaScript/23_Array_Sort.js
+++ JavaScript/23_Array_Sort.js
@@ -131,3 +131,41 @@ describe('sortStringsUp', () => {
         assert.sameOrderedMembers(actual, expected);
     });
 });
+
+
+/*
+Sorting String Descending Order:-
+
+'b'.localeCompare('a');
+*/
+
+//sortStringDown.js
+function sortStringsDown(array) {
+    array.sort((a,b)=>{
+        return b.localeCompare(a);
+    });   
+}
+
+module.exports = sortStringsDown;
+
+//test.js
+const { assert } = require('chai');
+const sortStringsDown = require('../sortStringsDown');
+
+describe('sortStringsDown', () => {
+    it('should mantain sort order for sorted strings', () => {
+        let actual = ["c", "b", "a"];
+        sortStringsDown(actual);
+        const expected = ["c", "b", "a"];
+        console.log({ actual, expected });
+        assert.sameOrderedMembers(actual, expected);
+    });
+
+    it('should fix sort order for unsorted strings', () => {
+        let actual = ['berries', 'oranges', 'apples', 'limes', 'lemons'];
+        sortStringsDown(actual);
+        const expected = ["oranges", "limes", "lemons", "berries", "apples"];
+        console.log({ actual, expected });
+        assert.sameOrderedMembers(actual, expected);
+    });
+});
