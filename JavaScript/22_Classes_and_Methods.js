39ad593b3c83fed10a35bfc27798980245c0b410
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-12-15 23:57:32 +0530

    initial commit

--- JavaScript/22_Classes_and_Methods.js
+++ JavaScript/22_Classes_and_Methods.js
@@ -216,3 +216,100 @@ describe('new Hero', () => {
         assert.equal(hero.rage, undefined);
     });
 });
+
+
+/*
+Calling Super Methods :-
+                    inheritance uses parent/child metaphors. 
+                    The class that has been extended is called the parent, while the class extending it is called the child.
+ */
+
+//Warrior.js
+const Hero = require('./Hero');
+
+class Warrior extends Hero {
+    constructor(){
+        super();
+        this.rage=0;
+    }
+
+    takeDamage(damage){
+        super.takeDamage(damage);
+        this.rage+=1;
+    }
+}
+
+module.exports = Warrior;
+
+//Hero.js
+class Hero {
+    constructor() {
+        this.health=50;
+    }
+    takeDamage(damage){
+        this.health-=damage;
+    }
+}
+
+module.exports = Hero;
+
+//test.js
+const { assert } = require('chai');
+const Warrior = require('../Warrior');
+const Hero = require('../Hero');
+
+describe('new Warrior', () => {
+    const warrior = new Warrior();
+
+    it('should have 50 health', () => {
+        assert.equal(warrior.health, 50);
+    });
+
+    it('should be a hero', () => {
+        assert(warrior instanceof Hero);
+    });
+
+    it('should have 0 rage', () => {
+        assert.equal(warrior.rage, 0);
+    });
+
+    describe('after taking 5 damage', () => {
+        before(() => {
+            warrior.takeDamage(5);
+        });
+
+        it('should have 45 health', () => {
+            assert.equal(warrior.health, 45);
+        });
+
+        it('should have 1 rage', () => {
+            assert.equal(warrior.rage, 1);
+        });
+
+        describe('after taking 15 more damage', () => {
+            before(() => {
+                warrior.takeDamage(15);
+            });
+
+            it('should have 30 health', () => {
+                assert.equal(warrior.health, 30);
+            });
+
+            it('should have 2 rage', () => {
+                assert.equal(warrior.rage, 2);
+            });
+        });
+    });
+});
+
+describe('new Hero', () => {
+    const hero = new Hero();
+
+    it('should have 50 health', () => {
+        assert.equal(hero.health, 50);
+    });
+
+    it('should not have rage', () => {
+        assert.equal(hero.rage, undefined);
+    });
+});
