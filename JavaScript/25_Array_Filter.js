19fd11a5ac9f3917331399d62aa32d7069a6dc0e
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-12-16 11:56:28 +0530

    initial commit

--- JavaScript/25_Array_Filter.js
+++ JavaScript/25_Array_Filter.js
@@ -168,3 +168,35 @@ describe('firstThree', () => {
         assert.sameDeepMembers(expected, actual);
     });
 });
+
+//Unique Element
+//unique.js
+function unique(array) {
+    // const newArray=([...new Set(array)]);
+    // return newArray;
+
+    return array.filter((x,i)=>{
+        return (array.indexOf(x)===i);
+    });
+
+}
+module.exports = unique;
+
+//test.js
+const { assert } = require('chai');
+const unique = require('../unique');
+
+describe('unique', () => {
+    it('should filter a few strings', () => {
+        const expected = ['a', 'b', 'c'];
+        const actual = unique(['a', 'b', 'c', 'b', 'c']);
+        console.log({expected, actual});
+        assert.sameDeepMembers(expected, actual);
+    });
+    it('should filter a several numbers', () => {
+        const expected = [10,20,30,40,50];
+        const actual = unique([40,50,10,20,30,40,50,10,10,10,10]);
+        console.log({ expected, actual });
+        assert.sameDeepMembers(expected, actual);
+    });
+});
