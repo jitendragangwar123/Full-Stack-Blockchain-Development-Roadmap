f8f3ada8decb2b9d60445e5e4f8841817581591d
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-12-16 12:31:59 +0530

    initial commit

--- JavaScript/26_Array_ Reduce.js
+++ JavaScript/26_Array_ Reduce.js
@@ -33,3 +33,35 @@ describe('sum', function () {
         assert.equal(sum([1,2,3,4,5,6,7,8,9,10]), 55);
     });
 });
+
+
+//Largest Value 
+//largest.js
+// numbers is an array full of numbers
+// let's find the largest and return it
+// i.e. [2,3,5,1,4] => 5
+function largest(numbers) {
+    return numbers.reduce((accumulator, currentValue) => {
+        return accumulator>currentValue ? accumulator:currentValue; // <-- determine largest value
+    });
+}
+
+module.exports = largest;
+
+//test.js
+const largest = require('../largest');
+const { assert } = require('chai');
+
+describe('largest', function () {
+    it('should take the largest of 1 element', () => {
+        assert.equal(largest([1]), 1);
+    });
+
+    it('should take the largest of 5 elements', () => {
+        assert.equal(largest([3,4,1,2,5]), 5);
+    });
+
+    it('should take the largest of 10 elements', () => {
+        assert.equal(largest([2,8,4,10,1,9,3,5,6,7]), 10);
+    });
+});
