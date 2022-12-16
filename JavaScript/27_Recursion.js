419f49b0c4ba652d7fd6cfbd5a2ea7bf22875a7a
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-12-17 00:49:14 +0530

    initial commit

--- JavaScript/27_Recursion.js
+++ JavaScript/27_Recursion.js
@@ -75,3 +75,47 @@ describe('factorial', () => {
         });
     });
 });
+
+
+/*
+Walk Nodes:-
+          Let's create a function that will find the last node in a list of nodes.
+A node will have two properties:
+
+id: A unique identifier
+next: A reference to another node
+
+// given a node, we can find node2 by using next
+const node2 = node.next;
+
+// node2 has its own id and next node reference
+console.log(node2); // {id: 2, next: {…}}
+
+// we can find node3 by using next on node2
+const node3 = node2.next;
+
+// node3 also has its own id and next node reference
+console.log(node3); // {id: 3, next: {…}}
+
+*/
+
+//walk.js
+function walk(node) {
+    if(!node.next){
+        return node;;
+    }
+}
+
+module.exports = walk;
+
+//test.js
+const walk = require('../walk');
+const { assert } = require('chai');
+
+describe('Walk', () => {
+    describe('base case', () => {
+        it('return the node', () => {
+            assert.deepEqual(walk({ id: 5 }), { id: 5 });
+        });
+    });
+});
