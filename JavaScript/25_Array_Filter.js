f25f7dd8e2bb59daa2aa495406c44de7ca0b46e3
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-12-16 11:21:08 +0530

    initial commit

--- JavaScript/25_Array_Filter.js
+++ JavaScript/25_Array_Filter.js
@@ -139,3 +139,32 @@ describe('topStudents', () => {
         assert.sameDeepMembers(expected, actual);
     });
 });
+
+
+//Filtering By Index
+//firstThree.js
+function firstThree(array) {
+    return array.filter((x,i)=>{
+        return (i<3);
+    });
+}
+module.exports = firstThree;
+
+//test.js
+const { assert } = require('chai');
+const firstThree = require('../firstThree');
+
+describe('firstThree', () => {
+    it('should filter a few strings', () => {
+        const expected = ['a', 'b', 'c'];
+        const actual = firstThree(['a', 'b', 'c', 'd', 'e']);
+        console.log({expected, actual});
+        assert.sameDeepMembers(expected, actual);
+    });
+    it('should filter a several numbers', () => {
+        const expected = [10,20,30];
+        const actual = firstThree([10,20,30,40,50,60,70,80,90,100]);
+        console.log({ expected, actual });
+        assert.sameDeepMembers(expected, actual);
+    });
+});
