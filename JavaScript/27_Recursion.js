543b6ed314a400587ebddcadf3a1dd6a78bac452
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-12-17 01:35:54 +0530

    initial commit

--- JavaScript/27_Recursion.js
+++ JavaScript/27_Recursion.js
@@ -119,3 +119,63 @@ describe('Walk', () => {
         });
     });
 });
+
+//Find the Last Node
+//walk.js
+function walk(node) {
+    if(!node.next){
+        return node;;
+    }
+    else{
+        return walk(node.next);
+    }
+}
+
+module.exports = walk;
+
+//text.js
+const walk = require('../walk');
+const { assert } = require('chai');
+
+describe('Walk', () => {
+    describe('base case', () => {
+        it('return the node', () => {
+            assert.deepEqual(walk({ id: 5 }), { id: 5 });
+        });
+    });
+
+    describe('recursive', () => {
+        it('return the last node in a list of 2', () => {
+            const node = { 
+                id: 1, 
+                next: { id: 2 }
+            }
+            assert.deepEqual(walk(node), { id: 2 });
+        });
+
+        it('return the last node in a list of 3', () => {
+            const node = {
+                id: 1,
+                next: {
+                    id: 2,
+                    next: { id: 3 }
+                }
+            }
+            assert.deepEqual(walk(node), { id: 3 });
+        });
+
+        it('return the last node in a list of 4', () => {
+            const node = {
+                id: 1,
+                next: {
+                    id: 2,
+                    next: { 
+                        id: 3,
+                        next: { id: 4 }
+                    }
+                }
+            }
+            assert.deepEqual(walk(node), { id: 4 });
+        });
+    });
+});
