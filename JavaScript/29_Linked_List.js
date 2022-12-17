2d13a4ad0bc76eb44a0a6cc65131f441c46c08d7
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-12-18 00:56:38 +0530

    initial commit

--- JavaScript/29_Linked_List.js
+++ JavaScript/29_Linked_List.js
@@ -256,3 +256,135 @@ describe('LinkedList instance', () => {
     });
 });
 
+
+/*indexOf :-
+const node1 = new Node(4);
+const node2 = new Node(3);
+const linkedList = new LinkedList();
+
+linkedList.addLast(node1); // node1 
+linkedList.addLast(node2); // node1 --> node2
+
+console.log(linkedList.indexOf(node1)); // 0
+console.log(linkedList.indexOf(node2)); // 1
+*/
+
+//LinkedList.js
+class LinkedList {
+    constructor(){
+        this.head=null;
+    }
+
+    addFirst(node){
+        node.next=this.head;
+        this.head=node;
+    }
+
+    addLast(node){
+        if(!this.head){
+            this.head=node;
+            return;
+        }
+
+        let ref=this.head;
+        while(ref.next){
+            ref=ref.next;
+        }
+        ref.next=node;
+    }
+
+    indexOf(node){
+        let idx=0;
+        let ptr=this.head;
+        do{
+            if(ptr===node){
+                return idx;
+            }
+            ptr=ptr.next;
+            idx++;
+        }
+        while(ptr)
+    }
+}
+
+module.exports = LinkedList;
+
+//Node.js
+class Node {
+    constructor(data){
+        this.data=data;
+        this.next=null;
+    }
+}
+
+module.exports = Node;
+
+//test.js
+const LinkedList = require('../LinkedList');
+const Node = require('../Node');
+const { assert } = require('chai');
+
+describe('LinkedList instance', () => {
+    const list = new LinkedList();
+
+    it('should start with a null head', () => {
+        assert.strictEqual(list.head, null);
+    });
+
+    describe('after adding a new node last', () => {
+        const node1 = new Node(1);
+        before(() => {
+            list.addLast(node1);
+        });
+
+        it('should set the head to the new node', () => {
+            assert.equal(list.head, node1);
+        });
+
+        it('should find the index at 0', () => {
+            assert.equal(list.indexOf(node1), 0)
+        });
+
+        describe('after adding another node first', () => {
+            const node2 = new Node(2);
+            before(() => {
+                list.addFirst(node2);
+            });
+
+            it('should set the head to the new node', () => {
+                assert.equal(list.head, node2);
+            });
+
+            it('should set the next on the new head', () => {
+                assert.equal(node2.next, node1);
+            });
+
+            it('should find the index at 0', () => {
+                assert.equal(list.indexOf(node2), 0)
+            });
+
+            it('should find the first node at index 1', () => {
+                assert.equal(list.indexOf(node1), 1)
+            });
+
+            describe('after adding another node last', () => {
+                const node3 = new Node(3);
+                before(() => {
+                    list.addLast(node3);
+                });
+
+                it('should keep the head as the previous node', () => {
+                    assert.equal(list.head, node2);
+                });
+
+                it('should set this node to the end', () => {
+                    assert.equal(list.head.next.next, node3);
+                });
+
+                it('should find the index at 2', () => {
+                    assert.equal(list.indexOf(node3), 2)
+                });
+            });
+        });
+    });
+}); 
