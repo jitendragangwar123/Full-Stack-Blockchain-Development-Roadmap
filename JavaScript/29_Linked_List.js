231fca682e25ed54fcc0ff61ed9cbd84c77f9f02
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-12-17 14:45:55 +0530

    initial commit

--- JavaScript/29_Linked_List.js
+++ JavaScript/29_Linked_List.js
@@ -154,3 +154,105 @@ describe('LinkedList instance', () => {
         });
     });
 }); 
+
+
+/*
+Add Last Node:-
+*/
+
+//LinkedList.js
+class LinkedList {
+    constructor(){
+        this.head=null;
+    }
+
+    addFirst(node){
+        node.next=this.head;
+        this.head=node;
+    }
+
+    addLast(node){
+        if(!this.head){
+            this.head=node;
+            return;
+        }
+
+        let ref=this.head;
+        while(ref.next){
+            ref=ref.next;
+        }
+        ref.next=node;
+        
+    }
+}
+
+module.exports = LinkedList;
+
+//Node.js
+class Node {
+    constructor(data){
+        this.data=data;
+        this.next=null;
+    }
+}
+
+module.exports = Node;
+
+//test.js
+const LinkedList = require('../LinkedList');
+const Node = require('../Node');
+const { assert } = require('chai');
+
+describe('LinkedList instance', () => {
+    const list = new LinkedList();
+
+    it('should start with a null head', () => {
+        assert.strictEqual(list.head, null);
+    });
+
+    describe('after adding a new node last', () => {
+        const node1 = new Node(1);
+        before(() => {
+            list.addLast(node1);
+        });
+
+        it('should set the head to the new node', () => {
+            assert.equal(list.head, node1);
+        });
+
+        describe('after adding another node first', () => {
+            const node2 = new Node(2);
+            before(() => {
+                list.addFirst(node2);
+            });
+
+            it('should set the head to the new node', () => {
+                assert.equal(list.head, node2);
+            });
+
+            it('should set the next on the new head', () => {
+                assert.equal(node2.next, node1);
+            });
+
+            describe('after adding another node last', () => {
+                const node3 = new Node(3);
+                before(() => {
+                    list.addLast(node3);
+                });
+
+                it('should keep the head as the previous node', () => {
+                    assert.equal(list.head, node2);
+                });
+
+                it('should set this node after the middle node', () => {
+                    assert.equal(node1.next, node3);
+                });
+
+                it('should set this node to the end', () => {
+                    assert.equal(node3.next, null);
+                });
+            });
+        });
+    });
+});
+
