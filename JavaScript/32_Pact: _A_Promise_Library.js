837e67ab2bc5052676978154d44ddb6e36f01f5a
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-12-19 02:25:30 +0530

    initial commit

--- JavaScript/32_Pact: _A_Promise_Library.js
+++ JavaScript/32_Pact: _A_Promise_Library.js
@@ -222,4 +222,284 @@ describe('Pact', function () {
 });
 
 
+//Pact.js
+const STATUS = {
+    PENDING: 0,
+    RESOLVED: 1,
+    REJECTED: 2,
+}
+
+class Pact {
+    constructor(fn) {
+        this.thenFns = [];
+        this.catchFns = [];
+        this.status = STATUS.PENDING;
+        this.resolve = (value) => {
+            this.resolvedValue = value;
+            this.status = STATUS.RESOLVED;
+            this.thenFns.forEach((fn) => fn(value));
+        }
+        this.reject = (value) => {
+            this.rejectedValue = value;
+            this.status = STATUS.REJECTED;
+            this.catchFns.forEach((fn) => fn(value));
+        }
+        fn(this.resolve, this.reject);
+    }
+    then(_then) {
+        if (this.status === STATUS.PENDING) {
+            this.thenFns.push(_then);
+        }
+        else if (this.status === STATUS.RESOLVED) {
+            _then(this.resolvedValue);
+        }
+    }
+    catch(_catch) {
+        if (this.status === STATUS.PENDING) {
+            this.catchFns.push(_catch);
+        }
+        else if (this.status === STATUS.REJECTED) {
+            _catch(this.rejectedValue);
+        }
+    }
+}
+
+module.exports = Pact;
+
+//test.js
+const Pact = require('../Pact');
+const { assert } = require('chai');
+
+describe('Pact', function () {
+    describe('immediate resolve', () => {
+        let pact;
+        before(() => {
+            pact = new Pact((resolve, reject) => {
+                resolve(42);
+            });
+        });
+
+        it('should run the then callback even after resolving', (done) => {
+            pact.then((val) => {
+                assert.equal(val, 42);
+                done();
+            });
+        }).timeout(250);
+    });
+
+    describe('immediate catch', () => {
+        let pact;
+        before(() => {
+            pact = new Pact((resolve, reject) => {
+                reject(42);
+            });
+        });
+
+        it('should run the catch callback even after resolution', (done) => {
+            pact.catch((val) => {
+                assert.equal(val, 42);
+                done();
+            });
+        }).timeout(250);
+    });
+
+    describe('asynchronous compatibility (previous tests)', () => {
+        it('should call a .then function with the resolve value after a timeout', (done) => {
+            let value;
+            new Pact((resolve, reject) => {
+                setTimeout(() => {
+                    resolve(42);
+                }, 100);
+            }).then((resolved) => {
+                assert.equal(resolved, 42);
+                done();
+            });
+        });
+
+        it('should call a .catch function with the reject value after a timeout', (done) => {
+            let value;
+            new Pact((resolve, reject) => {
+                setTimeout(() => {
+                    reject(42);
+                }, 100);
+            }).catch((err) => {
+                assert.equal(err, 42);
+                done();
+            });
+        });
+    });
+});
+
+
+//Chaining Callback
+//Pact.js
+const STATUS = {
+    PENDING: 0,
+    RESOLVED: 1,
+    REJECTED: 2,
+}
+
+class Pact {
+    constructor(fn) {
+        this.thenFns = [];
+        this.catchFns = [];
+        this.status = STATUS.PENDING;
+        this.resolve = (value) => {
+            this.resolvedValue = value;
+            this.status = STATUS.RESOLVED;
+            this.thenFns.forEach((fn) => fn(value));
+        }
+        this.reject = (value) => {
+            this.rejectedValue = value;
+            this.status = STATUS.REJECTED;
+            this.catchFns.forEach((fn) => fn(value));
+        }
+        fn(this.resolve, this.reject);
+    }
+    then(_then) {
+        if (this.status === STATUS.PENDING) {
+            return new Pact((resolve, reject) => {
+                this.thenFns.push((val) => {
+                    resolve(_then(val));
+                });
+            });
+        }
+        else if (this.status === STATUS.RESOLVED) {
+            _then(this.resolvedValue);
+        }
+    }
+    catch(_catch) {
+        if (this.status === STATUS.PENDING) {
+            this.catchFns.push(_catch);
+        }
+        else if (this.status === STATUS.REJECTED) {
+            _catch(this.rejectedValue);
+        }
+    }
+}
+
+module.exports = Pact;
+
+//test.js
+const Pact = require('../Pact');
+const { assert } = require('chai');
+
+describe('Pact', function () {
+    it('should return a chainable value after .then', () => {
+        let pact = new Pact((resolve, reject) => {
+            setTimeout(() => {
+                resolve(42);
+            }, 100);
+        }).then((val) => {
+            assert.equal(val, 42);
+        });
+        
+        assert(pact, "Expected .then to return a chainable pact!");
+        assert.equal(typeof pact.then, "function", "Expected .then to return a chainable pact!");
+    });
+
+    it('should handle value transformation', (done) => {
+        let pact = new Pact((resolve, reject) => {
+            setTimeout(() => {
+                resolve(42);
+            }, 100);
+        }).then((val) => {
+            assert.equal(val, 42);
+            return val * 2;
+        }).then((val) => {
+            assert.equal(val, 84);
+            return val * 2;
+        });
+
+        pact.then((val) => {
+            assert.equal(val, 168);
+            done();
+        });
+    });
+});
+
+//Chaining Promises
+//Pact.js
+const STATUS = {
+    PENDING: 0,
+    RESOLVED: 1,
+    REJECTED: 2,
+}
+
+class Pact {
+    constructor(fn) {
+        this.thenFns = [];
+        this.catchFns = [];
+        this.status = STATUS.PENDING;
+        this.resolve = (value) => {
+            this.resolvedValue = value;
+            this.status = STATUS.RESOLVED;
+            this.thenFns.forEach((fn) => fn(value));
+        }
+        this.reject = (value) => {
+            this.rejectedValue = value;
+            this.status = STATUS.REJECTED;
+            this.catchFns.forEach((fn) => fn(value));
+        }
+        fn(this.resolve, this.reject);
+    }
+    then(_then) {
+        if (this.status === STATUS.PENDING) {
+            return new Pact((resolve, reject) => {
+                this.thenFns.push((val) => {
+                    if (val instanceof Pact) {
+                        val.then((val) => resolve(_then(val)));
+                    }
+                    else {
+                        resolve(_then(val));
+                    }
+                });
+            });
+        }
+        else if (this.status === STATUS.RESOLVED) {
+            _then(this.resolvedValue);
+        }
+    }
+    catch(_catch) {
+        if (this.status === STATUS.PENDING) {
+            this.catchFns.push(_catch);
+        }
+        else if (this.status === STATUS.REJECTED) {
+            _catch(this.rejectedValue);
+        }
+    }
+}
+
+module.exports = Pact;
+
+//test.js
+const Pact = require('../Pact');
+const { assert } = require('chai');
+
+describe('Pact', function () {
+    it('should handle then chaining', (done) => {
+        let val1;
+        let val2;
+        let pact = new Pact((resolve, reject) => {
+            setTimeout(() => {
+                resolve(42);
+            }, 100);
+        }).then((val) => {
+            assert.equal(val, 42);
+            return new Pact((resolve, reject) => {
+                resolve(val * 2);
+            });
+        }).then((val) => {
+            assert.equal(val, 84);
+            return val * 2;
+        });
+
+        pact.then((val) => {
+            assert.equal(val, 168);
+            done();
+        });
+    });
+});
+
+
 
