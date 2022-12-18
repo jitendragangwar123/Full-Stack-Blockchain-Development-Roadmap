73df727597943c55a92f9bc084501ef6b9682214
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-12-19 02:09:06 +0530

    initial commit

--- JavaScript/32_Pact: _A_Promise_Library.js
+++ JavaScript/32_Pact: _A_Promise_Library.js
@@ -82,3 +82,50 @@ describe('Pact', function () {
         });
     });
 });
+
+//Pact.js
+class Pact {
+    constructor(fn) {
+        this.resolve = (value) => {
+            this.thenFn(value);
+        }
+        this.reject = () => {
+            
+        }
+        fn(this.resolve, this.reject);
+    }
+    then(_then) {
+        this.thenFn = _then;
+    }
+    catch() {
+        
+    }
+}
+
+module.exports = Pact;
+
+//test.js
+const Pact = require('../Pact');
+const { assert } = require('chai');
+
+describe('Pact', function () {
+    it('should call the .then callback', (done) => {
+        new Pact((resolve, reject) => {
+            setTimeout(resolve, 100);
+        }).then((resolved) => {
+            assert(true);
+            done();
+        });
+    });
+
+    it('should call the .then callback with the resolved value', (done) => {
+        new Pact((resolve, reject) => {
+            setTimeout(() => {
+                resolve(42);
+            }, 100);
+        }).then((resolved) => {
+            assert.equal(resolved, 42);
+            done();
+        });
+    });
+});
