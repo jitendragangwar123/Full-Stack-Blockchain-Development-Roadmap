9ebd9687b086b329fd4d70beabd1b14fd258451d
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-12-18 23:11:16 +0530

    initial commit

--- JavaScript/30_Callback_Function.js
+++ JavaScript/30_Callback_Function.js
@@ -184,3 +184,33 @@ describe('Dialog', () => {
         assert.equal(calledC, 1, "Third Callback was not called once after close");
     });
 });
+
+
+//For Each Callback
+//forEach.js
+function forEach(arr, callback) {
+    // for each element in the arr, run the callback, passing in the element
+    for(let i=0;i<arr.length;i++){
+        callback(arr[i],i);
+    }
+}
+
+module.exports = forEach;
+
+//test.js
+const forEach = require('../forEach');
+const { assert } = require('chai');
+
+describe('forEach', () => {
+    it('should loop over a function for every element', () => {
+        let sum = 0;
+        let iterations = 0;
+        forEach([1,2,3,4], (n, i) => {
+            assert.equal(i, iterations, "index should be supplied for each iteration");
+            iterations++;
+            sum += n;
+        });
+        assert.equal(iterations, 4, "Should be a total of four iterations for four elements");
+        assert.equal(sum, 10, "Should correctly sum four elements");
+    });
+});
