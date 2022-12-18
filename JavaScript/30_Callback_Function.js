46a16b4699f19e65e975400465b6d4aa0f3583ee
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-12-18 21:59:37 +0530

    initial commit

--- JavaScript/30_Callback_Function.js
+++ JavaScript/30_Callback_Function.js
@@ -40,3 +40,60 @@ describe('Run Callback', () => {
         assert(called, "Callback was not called");
     });
 });
+
+
+/*
+Asynchronous Callback:- 
+            In setTimeout, we give it a callback to run after a period of time (in milliseconds). 
+            The first argument to the function is the callback and the second argument is the number 
+            of milliseconds to wait before running the callback function.
+*/
+
+//runCallback.js
+/**
+ * Runs a callback function immediately
+ * @param {function} callbackFunction
+ */
+function runCallback(callbackFunction) {
+    setTimeout(()=>{
+         callbackFunction()
+    },1000);
+}
+
+module.exports = runCallback;
+
+//test.js
+const runCallback = require('../runCallback');
+const { assert } = require('chai');
+
+describe('Run Callback Async', () => {
+    it('should not run a function immediately', () => {
+        let called = false;
+        runCallback(() => {
+            called = true;
+        });
+        assert(!called, "Callback was called immediately");
+    });
+
+    it('should not run a function before 500 milliseconds', (done) => {
+        let called = false;
+        runCallback(() => {
+            called = true;
+        });
+        setTimeout(() => {
+            assert(!called, "Callback was called before 500 milliseconds");
+            done();
+        }, 500);
+    });
+
+    it('should run a function before 1500 milliseconds', (done) => {
+        let called = false;
+        runCallback(() => {
+            called = true;
+        });
+        setTimeout(() => {
+            assert(called, "Callback was not called before 1500 milliseconds");
+            done();
+        }, 1500);
+    });
+});
