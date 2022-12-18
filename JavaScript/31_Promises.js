95c8b02c26114a57eb93b56305c238c8e0293d1a
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2022-12-19 01:41:37 +0530

    initial commit

--- JavaScript/31_Promises.js
+++ JavaScript/31_Promises.js
@@ -311,3 +311,198 @@ describe('timer', () => {
         assert(true);
     }).timeout(1500);
 });
+
+
+/*
+Async Await:-
+      With these two keywords we can handle promises line-by-line like synchronous code:
+Ex:-
+async function getData() {
+    const result = await serverCall();
+
+    // this will not run until serverCall resolves/rejects
+    return result;
+}
+*/
+
+/*
+Catching Async/Await :-
+Ex:-
+async function getData() {
+    try {
+        const data = await callServer();
+    }
+    catch(ex) {
+        // this runs if the callServer promise rejects 
+        console.log(ex);
+    }
+}
+*/
+
+//handleResults.js
+const { getResults } = require('./lab');
+const { sendResults } = require('./messaging');
+const { logResponse, logError } = require('./logs');
+
+async function handleResults(patientId) {
+    try {
+        const results = await getResults(patientId);
+        const response = await sendResults(patientId, results);
+        await logResponse(response);
+    }
+    catch(ex) {
+        logError(ex);
+    }
+}
+
+module.exports = handleResults;
+
+//logs.js
+const __calls = [];
+const __errors = [];
+
+function logResponse(...args) {
+    __calls.push(args);
+    return new Promise((resolve, reject) => {
+        resolve();
+    });
+}
+
+function logError(...args) {
+    __errors.push(args);
+}
+
+module.exports = {
+    __calls,
+    __errors,
+    logResponse,
+    logError,
+};
+
+//messaging.js
+const __calls = [];
+const __response = { recieved: true };
+
+function sendResults(...args) {
+    __calls.push(args);
+    return new Promise((resolve, reject) => {
+        resolve(__response);
+    });
+}
+
+module.exports = {
+    __calls,
+    __response,
+    sendResults,
+};
+
+//labs.js
+const __calls = [];
+const __results = { negative: true };
+const __reset = () => { __calls.length = 0; }
+const __error = new Error("Results are not ready");
+
+function getResults(...args) {
+    __calls.push(args);
+    return new Promise((resolve, reject) => {
+        if (args[0] === 1) {
+            resolve(__results);
+        }
+        else {
+            reject(__error);
+        }
+    });
+}
+
+module.exports = {
+    __calls,
+    __reset,
+    __results,
+    __error,
+    getResults,
+};
+
+//test.js
+const lab = require('../lab');
+const messaging = require('../messaging');
+const logs = require('../logs');
+const handleResults = require('../handleResults');
+const { assert } = require('chai');
+
+describe('Lab Results', () => {
+    describe('first patient', () => {
+        const patientId = 2;
+        before(async () => {
+            await handleResults(patientId);
+        });
+
+        describe('calling the lab', () => {
+            it('should make one call', () => {
+                assert.equal(lab.__calls.length, 1);
+            });
+            it('should send one argument', () => {
+                assert.equal(lab.__calls[0].length, 1);
+            });
+            it('should send the patient id', () => {
+                assert.equal(lab.__calls[0][0], patientId);
+            });
+        });
+
+        describe('logging the error', () => {
+            it('should make one call', () => {
+                assert.equal(logs.__errors.length, 1);
+            });
+            it('should send one argument', () => {
+                assert.equal(logs.__errors[0].length, 1);
+            });
+            it('should send the error', () => {
+                assert.equal(logs.__errors[0][0], lab.__error);
+            });
+        });
+    });
+
+    describe('second patient', () => {
+        const patientId = 1;
+        before(async () => {
+            lab.__reset();
+            await handleResults(patientId);
+        });
+
+        describe('calling the lab', () => {
+            it('should make one call', () => {
+                assert.equal(lab.__calls.length, 1);
+            });
+            it('should send one argument', () => {
+                assert.equal(lab.__calls[0].length, 1);
+            });
+            it('should send the patient id', () => {
+                assert.equal(lab.__calls[0][0], patientId);
+            });
+        });
+
+        describe('sending the results', () => {
+            it('should make one call', () => {
+                assert.equal(messaging.__calls.length, 1);
+            });
+            it('should send two arguments', () => {
+                assert.equal(messaging.__calls[0].length, 2);
+            });
+            it('should send the patient id and lab results', () => {
+                assert.equal(messaging.__calls[0][0], patientId);
+                assert.equal(messaging.__calls[0][1], lab.__results);
+            });
+        });
+
+        describe('logging the response', () => {
+            it('should make one call', () => {
+                assert.equal(logs.__calls.length, 1);
+            });
+            it('should send one argument', () => {
+                assert.equal(logs.__calls[0].length, 1);
+            });
+            it('should send the response', () => {
+                assert.equal(logs.__calls[0][0], messaging.__response);
+            });
+        });
+    });
+});
