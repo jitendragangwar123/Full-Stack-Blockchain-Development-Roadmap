b2bbdfe1a994a2c64677b3cae4522afda4829877
jitendragangwar123 <gangwarjitendra2498@gmail.com>
2023-01-23 12:24:04 +0530

    token.js updated

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,6 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
+import "hardhat/console.sol";
+
+/// @title A contract for Token creation
+/// @author Jitendra Gangwar
+/// @notice For now, this contract just creating the Token 
+
 contract Token{
     string public tokenName="Jacob";
     string public tokenSymbol="JK";
@@ -15,6 +21,10 @@ contract Token{
     }
 
     function transfer(address _to,uint _amount) external{
+        //for debugging the smart contract
+        console.log("*Sender balance is %s Tokens*",balances[msg.sender]);
+        console.log("*Sender is sending %s Tokens to %s Address*",_amount,_to);
+
         require(balances[msg.sender]>=_amount,"Amount is not sufficient!");
         balances[msg.sender]-=_amount;
         balances[_to]+=_amount;
--- test/token.js
+++ test/token.js
@@ -2,7 +2,7 @@ const {expect}=require("chai");
 
 describe("Token Contract ",function(){
     let Token;
-    let JacobToken;
+    let jacobToken;
     let owner;
     let addr1;
     let addr2;
@@ -11,34 +11,52 @@ describe("Token Contract ",function(){
     beforeEach(async function(){
         Token=await ethers.getContractFactory("Token");
         [owner,addr1,addr2,...addrs]=await ethers.getSigners();
-        JacobToken= await Token.deploy();
+        jacobToken= await Token.deploy();
     });
 
     describe("Deployment",function(){
         it("Should set the right owner ",async function(){
-            expect(await JacobToken.owner()).to.equal(owner.address);
+            expect(await jacobToken.owner()).to.equal(owner.address);
         });
 
         it("Should assign the totalSupply to the owner ",async function(){
-            const totalBalance=await JacobToken.balanceOf(owner.address);
-            expect(await JacobToken.totalSupply()).to.equal(totalBalance);
+            const totalBalance=await jacobToken.balanceOf(owner.address);
+            expect(await jacobToken.totalSupply()).to.equal(totalBalance);
         });
     });
 
     describe("Transactions",function(){
         it("Should transfer token between accounts", async function(){
-            await JacobToken.transfer(addr1.address,10);
-            const addr1Balance=await JacobToken.balanceOf(addr1.address);
+            await jacobToken.transfer(addr1.address,10);
+            const addr1Balance=await jacobToken.balanceOf(addr1.address);
             expect(addr1Balance).to.equal(10);
 
-            await JacobToken.connect(addr1).transfer(addr2.address,5);
-            const addr2Balance=await JacobToken.balanceOf(addr2.address);
+            await jacobToken.connect(addr1).transfer(addr2.address,5);
+            const addr2Balance=await jacobToken.balanceOf(addr2.address);
             expect(addr2Balance).to.equal(5);
         });
+
+        it("Should fail if the sender have not enough balance",async function(){
+            const initialOwnerBalance=await jacobToken.balanceOf(owner.address);
+            await expect(jacobToken.connect(addr1).transfer(owner.address,1)).to.be.revertedWith("Amount is not sufficient!");
+            expect(await jacobToken.balanceOf(owner.address)).to.equal(initialOwnerBalance);
+        });
+
+        it("Should update balances after transactions",async function(){
+            const initialOwnerBalance=await jacobToken.balanceOf(owner.address);
+            await jacobToken.transfer(addr1.address,10);
+            await jacobToken.transfer(addr2.address,20);
+
+            const finalOwnerBalance=await jacobToken.balanceOf(owner.address);
+
+            expect(finalOwnerBalance).to.equal(initialOwnerBalance-30);
+
+            const addr1Balance=await jacobToken.balanceOf(addr1.address);
+            const addr2Balance=await jacobToken.balanceOf(addr2.address);
+            expect(addr1Balance).to.equal(10);
+            expect(addr2Balance).to.equal(20);
+        });
     });
-
-    
-
 });
 
 
@@ -48,25 +66,23 @@ describe("Token Contract ",function(){
 //         //console.log("Owner Object: ",owner);
 //         const Token= await ethers.getContractFactory("Token"); //Create Token Instance
         
-//         const JacobToken=await Token.deploy(); // deployment of token
-//         const ownerBalance=await JacobToken.balanceOf(owner.address);
+//         const jacobToken=await Token.deploy(); // deployment of token
+//         const ownerBalance=await jacobToken.balanceOf(owner.address);
 //         //console.log("Owner Address: ",owner.address);
 
-//         expect(await JacobToken.totalSupply()).to.equal(ownerBalance);
+//         expect(await jacobToken.totalSupply()).to.equal(ownerBalance);
 //     });
 
 //     it("Should transfer tokens between accounts ",async function(){
 //         const [owner,addr1,addr2]=await ethers.getSigners();
 //         const Token= await ethers.getContractFactory("Token"); //Create Token Instance
         
-//         const JacobToken=await Token.deploy(); // deployment of token
-//         await JacobToken.transfer(addr1.address,10);
-//         expect(await JacobToken.balanceOf(addr1.address)).to.equal(10);
+//         const jacobToken=await Token.deploy(); // deployment of token
+//         await jacobToken.transfer(addr1.address,10);
+//         expect(await jacobToken.balanceOf(addr1.address)).to.equal(10);
 
-//         await JacobToken.connect(addr1).transfer(addr2.address,5);
-//         expect(await JacobToken.balanceOf(addr2.address)).to.equal(5);
+//         await jacobToken.connect(addr1).transfer(addr2.address,5);
+//         expect(await jacobToken.balanceOf(addr2.address)).to.equal(5);
 //     });
-
-
 // });
 
